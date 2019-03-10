5eed5c9eec915880d7623bf4f89048dd070f71f0
Max Dauber <max@dauberfamily.com>
2019-03-10 15:19:11 -0500

    initial file commit

---
+++ src/algo_project2/Program2.java
@@ -0,0 +1,19 @@
+/**
+ * Created by tkalbar on 3/2/19.
+ */
+
+package algo_project2;
+
+public class Program2 {
+
+    public int constructIntensityGraph(int[][] image){
+        // TODO
+        return 0;
+    }
+
+    public int constructPrunedGraph(int[][] image){
+        // TODO
+        return 0;
+    }
+
+}
