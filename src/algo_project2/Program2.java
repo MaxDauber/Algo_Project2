ab1539e30ba3d06a8a35e6395144f3f471833857
Max Dauber <max@dauberfamily.com>
2019-03-25 13:01:34 -0500

    completed constructIntensityGraph
    brute force add of all edges between pixels in the graph

--- src/algo_project2/Program2.java
+++ src/algo_project2/Program2.java
@@ -1,19 +1,36 @@
-/**
- * Created by tkalbar on 3/2/19.
- */
+
 
 package algo_project2;
 
 public class Program2 {
 
     public int constructIntensityGraph(int[][] image){
-        // TODO
-        return 0;
+        int intensityWeight = 0;
+        //iterate through all pixels in graph
+        for(int r = 0; r < image.length; r++){
+            for(int c = 0; c < image[r].length; c++){
+                //if on right edge of graph, add intensity of pixel and its lower counterpart
+                if(c == image[r].length - 1 && !(r == image.length - 1)){
+                    intensityWeight += Math.abs(image[r][c] - image[r+1][c]);
+                }
+                //if on bottom edge of graph, add intensity of pixel and its right counterpart
+                else if (r == image.length - 1 && !(c == image[r].length - 1)){
+                    intensityWeight += Math.abs(image[r][c] - image[r][c+1]);
+                }
+                //otherwise as long as not lower right pixel add both down and right intensities
+                else if (!(r == image.length - 1) && !(c == image[r].length - 1)){
+                    intensityWeight += Math.abs(image[r][c] - image[r+1][c]);
+                    intensityWeight += Math.abs(image[r][c] - image[r][c+1]);
+                }
+            }
+        }
+        return intensityWeight;
     }
 
     public int constructPrunedGraph(int[][] image){
-        // TODO
-        return 0;
+        int prunedWeight = 0;
+
+        return prunedWeight;
     }
 
 }
